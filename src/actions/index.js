46d3eec236aae195352dba497c6d78e5076744b9
justindavisa <justindavisa@gmail.com>
2019-03-04 17:47:24 -0500

    MVP Complete, Inrement and Decrement are working

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,12 +13,12 @@ export const increment = count => {
   return {
     type: INCREMENT,
     payload: count
-  }
+  };
 };
 
 export const decrement = count => {
   return {
     type: DECREMENT,
     payload: count
-  }
+  };
 };
